9112ca14ed71844cfce7a14a745f014388c6e0fb
frank <franksavena@gmail.com>
2023-01-26 08:58:56 -0600

    simple validations

--- app/models/nft.rb
+++ app/models/nft.rb
@@ -1,4 +1,9 @@
 class Nft < ApplicationRecord
+
+    validates :name, :image, :description, :price, :for_sale, presence: true
+    validates :name, :image, :description, :price, :for_sale, uniqueness: true
+   
+
     belongs_to :user 
     has_many :reviews 
     has_many :users, through: :reviews
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+    validates :user_name, :password_digest, presence: true
+    validates :user_name, :email, uniqueness: true
+    validates :password_digest, numericality: {:greater_than_or_equal_to 4}
+
+
+
+
     has_many :reviews 
     has_many :nfts, through: :reviews
     has_many :nfts
